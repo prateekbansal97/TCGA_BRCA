ba7dc4abc1e024fbed21d5d557a37d197f572eed
Prateek Bansal <prateekbansal97@gmail.com>
2025-06-01 19:12:52 -0500

    Added load_clinical_phenotype after testing

--- utils/preprocessing.py
+++ utils/preprocessing.py
@@ -6,3 +6,10 @@ def load_rnaseq(file_path):
     df.index.name = 'sample'
     return df
 
+def load_clinical_phenotype(file_path):
+    df = pd.read_csv(file_path, sep='\t', index_col=0)
+    df = df[df['vital_status'].notna() & df['days_to_last_followup'].notna()]
+    df['event'] = df['vital_status'].apply(lambda x: 1 if x == 'DECEASED' else 0)
+    df['time'] = df['days_to_last_followup']
+    df.index.name = 'sample'
+    return df[['event', 'time']]
